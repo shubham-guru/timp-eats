3817b707cd829129a721dbe074a6853c072f8747
BhavyamMahajan <bhavyammahajan07@gmail.com>
2024-07-13 15:30:49 +0530

    razorpay setup

--- src/presentation/components/OrderSummary.tsx
+++ src/presentation/components/OrderSummary.tsx
@@ -1,5 +1,15 @@
 import { useEffect, useState } from "react";
-import { Col, Table, Typography, Flex, Divider, Button, message } from "antd";
+import {
+  Col,
+  Table,
+  Typography,
+  Flex,
+  Divider,
+  Button,
+  message,
+  Row,
+  Space,
+} from "antd";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import logo from "../../assets/logo.png";
@@ -12,8 +22,6 @@ import routes from "../../routes/routes";
 import { useNavigate } from "react-router-dom";
 import { removeAllProducts } from "../../redux/slice/cartSlice";
 import { timeZones } from "../../domain/constants/timeZones";
-// import { PayPalButtons, FUNDING } from "@paypal/react-paypal-js";
-import PayPalButton from "./PaymentButton";
 
 type IOrderSummaryType = {
   userData: formUserDataInterface;
@@ -32,8 +40,6 @@ const OrderSummary: React.FC<IOrderSummaryType> = ({ userData, isLoading }) => {
     PaymentTypes.PAYNOW
   );
   const [currency, setCurrency] = useState<string>("");
-  const [orderId, setOrderId] = useState("");
-  console.log("🚀 ~ orderId:", orderId)
 
   const currencySym = localStorage.getItem("currencySym");
   const timeZone = localStorage.getItem("timeZone");
@@ -45,7 +51,7 @@ const OrderSummary: React.FC<IOrderSummaryType> = ({ userData, isLoading }) => {
       setCurrency("EUR");
     } else if (timeZone?.includes(timeZones.USA)) {
       setCurrency("USD");
-    } else{
+    } else {
       setCurrency("USD");
     }
   }, [timeZone]);
@@ -68,7 +74,6 @@ const OrderSummary: React.FC<IOrderSummaryType> = ({ userData, isLoading }) => {
   );
 
   const totalPrice = calculateTotalPrice(productObj);
-  console.log("🚀 ~ totalPrice:", totalPrice)
 
   // Calculating Total Qty of Products in KGs
   const totalQty: any[] = productObj?.map(
@@ -131,28 +136,54 @@ const OrderSummary: React.FC<IOrderSummaryType> = ({ userData, isLoading }) => {
     },
   ];
 
-  const calculatePriceWithDelivery = () => {
+  const calculateDeliveryCharge = () => {
+    const deliverCharge = 49;
     if (currency === "USD") {
-      const checkoutAmt = (
-        quantitySum > 1 ? totalPrice : totalPrice + 45
-      ).toLocaleString("en-US", {
-        style: "currency",
-        currency: currency,
-      });
-      console.log("🚀 ~ calculatePriceWithDelivery ~ totalPrice:", checkoutAmt)
-      return checkoutAmt;
-    } else {
-      return totalPrice;
-    }
-    }
+      return quantitySum > 1 ? 0 : deliverCharge;
+    } else if (currency === "INR") {
+      return quantitySum >= 3 ? 0 : deliverCharge;
+    } else return 0;
+  };
+
+  const calculatePriceWithDelivery = () => {
+    if (currency === "USD")
+      return quantitySum > 1 ? totalPrice : totalPrice + 49;
+    else if (currency === "INR")
+      return quantitySum >= 3 ? totalPrice : totalPrice + 49;
+    else return totalPrice;
+  };
+
   const calculatePriceWithDeliverytoString = () => {
     if (currency === "USD") {
-      const checkoutAmt = quantitySum > 1 ? totalPrice : totalPrice + 45.00
+      const checkoutAmt = quantitySum > 1 ? totalPrice : totalPrice + 45.0;
       return checkoutAmt.toString();
     } else {
       return totalPrice;
     }
-    }
+  };
+
+  const verifyPayment = (reqBody: {
+    razorpay_payment_id: string;
+    razorpay_order_id: string;
+    razorpay_signature?: string;
+  }) => {
+    axios
+      .post(baseUrl + "/paymentConfirmation", {
+        ...reqBody,
+        currency,
+        amount: parseFloat(calculatePriceWithDelivery()),
+      })
+      .then((res) => {
+        messageApi.success(res.data.message);
+        setTimeout(() => {
+          dispatch(removeAllProducts());
+          navigate(routes.HOME);
+        }, 4000);
+      })
+      .catch((err) => {
+        messageApi.error(err.response.data.message);
+      });
+  };
 
   const handleCheckout = async () => {
     const completeAddress = userData.complete_address || {};
@@ -173,99 +204,101 @@ const OrderSummary: React.FC<IOrderSummaryType> = ({ userData, isLoading }) => {
     );
 
     if (allValuesFilled && allAddressValuesFilled) {
-
-      const numberString = calculatePriceWithDelivery();
+      const totalAmt = parseFloat(calculatePriceWithDelivery());
       // const numberString = str.replace(/[^\d.]/g, '');
-      const totalAmt = parseFloat(numberString);
-      isLoading(true)
-      await axios.post(baseUrl + "/order", {
+
+      isLoading(true);
+      await axios
+        .post(baseUrl + "/order", {
           user: userData,
           order: {
-              payment_mode: selectedPayment,
-              order_detail: productObj,
-              total_price: totalAmt,
-              tax: 0,
-              delievery_charge: quantitySum >= 3 ? "FREE" : "₹49",
-              status: "cart"
+            payment_mode: selectedPayment,
+            order_detail: productObj,
+            total_price: totalAmt,
+            tax: 0,
+            delievery_charge: calculateDeliveryCharge(),
+            status: "cart",
           },
-          amount: totalAmt
-      }).then((res) => {
-          console.log("🚀 ~ handleCheckout ~ res:", res)
-          const resOrder = res.data.order
-          if (res?.data) isLoading(false)
+          amount: totalAmt,
+          currency,
+        })
+        .then((res) => {
+          const resOrder = res.data.order;
+          if (res?.data) isLoading(false);
           if (selectedPayment === PaymentTypes.COD) {
-              messageApi.success("Order Placed Successfully")
-              setTimeout(() => {
-                  dispatch(removeAllProducts())
-                  navigate(routes.HOME)
-              }, 3000)
-          }
-      else if (selectedPayment === PaymentTypes.PAYNOW) {
-          const options = {
+            messageApi.success("Order Placed Successfully");
+            setTimeout(() => {
+              dispatch(removeAllProducts());
+              navigate(routes.HOME);
+            }, 3000);
+          } else if (selectedPayment === PaymentTypes.PAYNOW) {
+            const options = {
               razorpay_key,
               amount: resOrder.amount,
-              currency: currency,
+              currency,
               name: "Timp Eats",
               description: "Payment for order",
               image: logo,
               order_id: resOrder.id,
-              callback_url: baseUrl + "/getPaymentConfirmation",
+              handler: verifyPayment,
+              retry: { enabled: false },
               prefill: {
-                  name: userData.full_name,
-                  email: userData.email,
-                  contact: userData.phone_number
+                name: userData.full_name,
+                email: userData.email,
+                contact: userData.phone_number,
               },
               notes: {
-                  "address": "Razorpay Corporate Office"
+                address: "Razorpay Corporate Office",
               },
               theme: {
-                  "color": "#121212"
-              }
-          };
-          const razor = (window).Razorpay(options)
-          razor.open();
-      }
-    }).catch((err) => {
-      messageApi.error("Something went wrong")
-      console.log("🚀 ~ handleCheckout ~ err:", err)
-  })
+                color: "#121212",
+              },
+            };
+
+            const razor = window.Razorpay(options);
+            razor.open();
+            razor.on("payment.failed", (res: any) => {
+              console.log("on err : ", res);
+              verifyPayment({
+                razorpay_order_id: res.error.metadata.order_id,
+                razorpay_payment_id: res.error.metadata.payment_id,
+              });
+            });
+          }
+        })
+        .catch((err) => {
+          messageApi.error("Something went wrong");
+          isLoading(false);
+        });
     } else {
       messageApi.error("Kindly fill all the details in the form");
     }
   };
 
   const dataSource = productsData;
-  const handleSuccess = (message:any) => {
-    console.log('Payment successful!',message);
-  };
-
-  const handleError = (error: any) => {
-    console.error('Payment failed!', error);
-  };
-
 
   return (
     <Col className="order-summary-main-col glassmorphism-effect">
       <Typography.Text className="heading-md">Order Summary</Typography.Text>
       {contextHolder}
       <Table dataSource={dataSource} columns={columns} pagination={false} />
-      {
-        currency === "INR" ?  <Flex className="order-summary-flex" justify="space-between">
-        <Typography.Text className="order-summary-footer-text">
-          Delivery
-        </Typography.Text>
-        {currencySym === "₹" ? (
-          <Typography.Text className="order-summary-footer-text free-del-text">
-            {quantitySum >= 3 ? "FREE" : "₹49"}
+      {currency === "INR" ? (
+        <Flex className="order-summary-flex" justify="space-between">
+          <Typography.Text className="order-summary-footer-text">
+            Delivery
           </Typography.Text>
-        ) : (
-          <Typography.Text className="order-summary-footer-text free-del-text">
-            FREE
-          </Typography.Text>
-        )}
-      </Flex> : null
-      }
-     
+          {currencySym === "₹" ? (
+            <Typography.Text className="order-summary-footer-text free-del-text">
+              {calculateDeliveryCharge() || "FREE"}
+            </Typography.Text>
+          ) : (
+            <Typography.Text className="order-summary-footer-text free-del-text">
+              FREE
+            </Typography.Text>
+          )}
+        </Flex>
+      ) : null}
+
       <br />
       {/* {currencySym === "₹" ?? (
         <Typography.Text className="order-summary-footer-text">
@@ -280,7 +313,6 @@ const OrderSummary: React.FC<IOrderSummaryType> = ({ userData, isLoading }) => {
       )} */}
       <Divider />
       <Flex vertical gap={15}>
-        
         {currencySym === "₹" ?? (
           <Flex
             className={`glassmorphism-effect ${
@@ -311,75 +343,76 @@ const OrderSummary: React.FC<IOrderSummaryType> = ({ userData, isLoading }) => {
           </Flex>
         )}
 
-       { currencySym === "$" ?  
-       <Flex
-          className={`glassmorphism-effect ${
-            selectedPayment === PaymentTypes.PAYNOW ? "selected-payment" : "" }`}
-          style={{ padding: "3%" }}
-          align="center"
-          onClick={() => setSelectedPayment(PaymentTypes.PAYNOW)}
-        >
-          <Col span={20}>
-            <Typography.Title className="footer-title">
-              Pay Now
-            </Typography.Title>
-            {selectedPayment === PaymentTypes.PAYNOW ? (
+        <Space direction="vertical">
+          {currency === "INR" && (
+            <Row
+              className={`glassmorphism-effect ${
+                selectedPayment === PaymentTypes.COD ? "selected-payment" : ""
+              }`}
+              align="middle"
+              style={{ padding: "3%", cursor: "pointer" }}
+              onClick={() => setSelectedPayment(PaymentTypes.COD)}
+            >
+              <Col span={20}>
+                <Typography.Title className="footer-title">
+                  COD
+                </Typography.Title>
+                <Typography.Text>
+                  <i>It's our first step of believing in each other. </i>
+                </Typography.Text>
+              </Col>
+
+              <Col>
+                <Typography.Text
+                  className="order-summary-footer-text"
+                  style={{ color: "green" }}
+                >
+                  {currencySym}
+                  {calculatePriceWithDelivery()}
+                </Typography.Text>
+              </Col>
+            </Row>
+          )}
+          <Row
+            className={`glassmorphism-effect ${
+              selectedPayment === PaymentTypes.PAYNOW ? "selected-payment" : ""
+            }`}
+            align={"middle"}
+            style={{ padding: "3%", cursor: "pointer" }}
+            onClick={() => setSelectedPayment(PaymentTypes.PAYNOW)}
+          >
+            <Col span={20}>
+              <Typography.Title className="footer-title">
+                Pay Now
+              </Typography.Title>
               <Typography.Text>
                 <i>Looks like you already trust us :) </i>
               </Typography.Text>
-            ) : null}
-          </Col>
-          <Col>
-            {selectedPayment === PaymentTypes.PAYNOW ? (
+            </Col>
+            <Col>
               <Typography.Text
                 className="order-summary-footer-text"
                 style={{ color: "green" }}
               >
+                {currencySym}
                 {calculatePriceWithDelivery()}
               </Typography.Text>
-            ) : null}
-          </Col>
-        </Flex> : 
-         <Flex
-         className={`glassmorphism-effect ${
-           selectedPayment === PaymentTypes.COD ? "selected-payment" : "" }`}
-         style={{ padding: "3%" }}
-         align="center"
-         onClick={() => setSelectedPayment(PaymentTypes.COD)}
-       >
-         <Col span={20}>
-           <Typography.Title className="footer-title">
-             COD
-           </Typography.Title>
-             <Typography.Text>
-                <i>It's our first step of believing in each other. </i>
-             </Typography.Text>
-         </Col>
-         <Col>
-         <Typography.Text
-               className="order-summary-footer-text"
-               style={{ color: "green" }}>
-                {timeZone === timeZones.INDIA && currencySym}
-               {calculatePriceWithDelivery()}
-             </Typography.Text>
-         </Col>
-       </Flex> }
-        
+            </Col>
+          </Row>
+        </Space>
       </Flex>
       <br />
-      { timeZone !== timeZones.INDIA ? <PayPalButton amount={calculatePriceWithDeliverytoString()} onSuccess={handleSuccess} onError={handleError}/> : 
-        <Button
+
+      <Button
         type="primary"
         className="primary-us-btn cart-btn"
         onClick={handleCheckout}
       >
-        Pay & Checkout
-      </Button> }
-    
-      {/* {orderId && <button onClick={capturePayment}>Capture Payment</button>} */}
+        {selectedPayment === "razorpay" && "Pay & "}
+        Checkout
+      </Button>
     </Col>
   );
 };
 
 export default OrderSummary;
-
